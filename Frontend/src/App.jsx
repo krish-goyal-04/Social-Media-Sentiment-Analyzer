68ee6bccf588dae72d6b0632ead5004262466853
Krish Goyal <krishofficial4aug@gmail.com>
2025-08-19 00:45:00 +0530

    HomePage UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import HomePage from "./components/Home/HomePage"
 
 const App = ()=>{
     return(
         <div className=" min-h-screen bg-neutral-900">
             <Header />
+            <HomePage />
         </div>
     )
 }
--- Frontend/src/components/Home/InitialDisplay.jsx
+++ Frontend/src/components/Home/InitialDisplay.jsx
@@ -1,7 +1,11 @@
 const InitialDisplay = ()=>{
     return(
-        <div >
-            
+        <div className=" p-6" >
+            <div>
+                <div className="text-4xl font-bold">
+                    Get <span>Real-Time</span> Analysis with <span>TrendPulse</span>
+                </div>
+            </div>
         </div>
     )
 }
